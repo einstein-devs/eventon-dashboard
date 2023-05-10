a498f5752bb413ec42cf7c0273f9cf437a8bb677
tiago <tiago.lima633@gmail.com>
2023-05-10 20:31:27 -0300

    EE-32

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,4 +1,4 @@
-import style from "../styles/styles.module.css"
+import style from "../styles/login.module.css"
 
 export default function telaLogin() {
     return(
@@ -7,13 +7,15 @@ export default function telaLogin() {
             <div className={style.wrapLogin}>
                     <div className={style.loginForm}>
                         <div className={style.loginFormTitle}>
+                            <span className={style.entrar} >Entrar</span>
                             <input className={style.wrapInput} type="text" name="ra" placeholder="Login"/>
                             <br />
                             <input className={style.wrapInput} type="password" name="senha" placeholder="Senha"/>
-
                             <br />
                             <div className={style.containerLoginFormBtn}>
-                            <button className={style.loginFormBtn}>entrar</button>
+                            <button className={style.loginFormBtn}>Acessar</button>
+                            <br />
+                            <a href="" className={style.esqueceuSenha}>Esqueceu sua senha?</a>
                             </div>
                     </div>
                 </div>
