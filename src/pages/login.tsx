53330e86e5ac7d0ad475e8e3cdcf1264f2445c93
tiago <tiago.lima633@gmail.com>
2023-05-13 18:39:02 -0300

    EE-32

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,27 +1,28 @@
 import style from "../styles/login.module.css"
+import Link from 'next/link'
 
 export default function telaLogin() {
-    return(
-    <div className={style.limiter}>
-        <div className={style.containerLogin}>
-            <div className={style.wrapLogin}>
+    return (
+        <div className={style.limiter}>
+            <div className={style.containerLogin}>
+                <div className={style.wrapLogin}>
                     <div className={style.loginForm}>
                         <div className={style.loginFormTitle}>
                             <span className={style.entrar} >Entrar</span>
-                            <input className={style.wrapRa} type="text" name="ra" placeholder="Digite seu RA"/>
+                            <input className={style.wrapInput} type="text" name="ra" placeholder="Digite seu RA" />
                             <br />
-                            <input className={style.wrapSenha} type="password" name="senha" placeholder="Digie sua senha"/>
+                            <input className={style.wrapInput} type="password" name="senha" placeholder="Digie sua senha" />
                             <br />
                             <div className={style.containerLoginFormBtn}>
-                            <button className={style.loginFormBtn}>Acessar</button>
-                            <br />
-                            <a href="" className={style.esqueceuSenha}>Esqueceu sua senha?</a>
+                                <button className={style.loginFormBtn}>Acessar</button>
+                                <br />
+                                <Link href="./esqueci-senha" className={style.esqueceuSenha} >Esqueceu sua senha?</Link>
                             </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    )   
-    
+    )
+
 }
